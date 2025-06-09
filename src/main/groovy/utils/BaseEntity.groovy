affce0504070001330aca32e730888c11d337e04
rapha-asaas <raphael.diogenes@asaas.com.br>
2025-06-09 14:07:32 -0400

    Adicionando arquivo BaseEntity

---
+++ src/main/groovy/utils/BaseEntity.groovy
@@ -0,0 +1,12 @@
+package com.asaas.mini.utils
+
+abstract class BaseEntity {    
+
+    Date dateCreated
+    Date lastUpdated
+    Boolean deleted = false
+
+    static mapping = {
+        tablePerHierarchy false
+    }
+}
