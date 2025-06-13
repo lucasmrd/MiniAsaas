0976072104d32009daaf6fbb0f1f0ebd9a10d14e
Lucas Miranda <lucas.mranda1@gmail.com>
2025-06-13 12:38:05 -0300

    Adicionando lista de idades de emancipação e Boolean emancipated em Customer

--- grails-app/domain/com/asaas/mini/Customer.groovy
+++ grails-app/domain/com/asaas/mini/Customer.groovy
@@ -2,4 +2,8 @@ package com.asaas.mini
 
 import com.asaas.mini.utils.BaseEntityPersonInfo
 
-public class Customer extends BaseEntityPersonInfo { }
+    public class Customer extends BaseEntityPersonInfo {
+
+        static List<Integer> EMANCIPATION_AGE_LIST = [16, 17]
+        Boolean emancipated
+    }
