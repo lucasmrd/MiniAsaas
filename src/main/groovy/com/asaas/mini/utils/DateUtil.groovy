638809c1584f18c308da08be0f39f39cc259139b
Lucas Miranda <lucas.mranda1@gmail.com>
2025-06-13 12:34:49 -0300

    Adicionando métodos de validação de data em DateUtil e refatorando validação de BirthDate

--- src/main/groovy/com/asaas/mini/utils/DateUtil.groovy
+++ src/main/groovy/com/asaas/mini/utils/DateUtil.groovy
@@ -13,4 +13,48 @@ public class DateUtil {
             return null
         }
     }
+
+    public static Boolean isValidBirthDate(String birthDate) {
+        birthDate = fromString(birthDate)
+
+        int minDate = 16
+
+        if (!birthDate) return false
+
+        Date limitDate = fromString("01/01/1900")
+        if (birthDate.before(limitDate)) return false
+
+        Date today = new Date()
+        if (birthDate.after(today)) return false
+
+        Calendar birthCal = Calendar.getInstance()
+        birthCal.time = birthDate
+
+        Calendar todayCal = Calendar.getInstance()
+        todayCal.time = today
+
+        int age = todayCal.get(Calendar.YEAR) - birthCal.get(Calendar.YEAR)
+
+        int currentMonth = todayCal.get(Calendar.MONTH)
+        int birthMonth = birthCal.get(Calendar.MONTH)
+        int currentDay = todayCal.get(Calendar.DAY_OF_MONTH)
+        int birthDay = birthCal.get(Calendar.DAY_OF_MONTH)
+
+        if (currentMonth < birthMonth || (currentMonth == birthMonth && currentDay < birthDay)) age--
+
+        if (!(age >= minDate)) return false
+
+        return true
+    }
+
+    public static Boolean isValidCompanyCreationDate(String companyCreationDate) {
+        companyCreationDate = fromString(companyCreationDate)
+
+        if (!companyCreationDate) return false
+
+        Date today = new Date()
+        if (companyCreationDate.after(today)) return false
+
+        return true
+    }
 }
