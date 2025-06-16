2783bb79a3d264519ca16b36351f93ce4aa0f8d2
rapha-asaas <raphael.diogenes@asaas.com.br>
2025-06-16 12:59:09 -0400

    Simplificando método dos dados do customer

--- grails-app/assets/javascripts/customer/CreateCustomerController.js
+++ grails-app/assets/javascripts/customer/CreateCustomerController.js
@@ -4,9 +4,14 @@ function CreateCustomerController(reference) {
     const addressComplementInput = reference.querySelector('.js-address-complement-input');
     const cityInput = reference.querySelector('.js-city-input');
     const stateInput = reference.querySelector('.js-state-select');
+    const personTypeSelect = reference.querySelector('.js-person-type-select');
+    const companyTypeSelect = reference.querySelector('.js-company-type-select');
+    const personTypeFields = reference.querySelector('.js-person-type-fields');
+    const companyTypeFields = reference.querySelector('.js-company-type-fields');
 
     const init = function() {
         bindPostalCodeInput();
+        bindCustomerTypeSelect();
     };
 
     const bindPostalCodeInput = function() {
@@ -15,12 +20,44 @@ function CreateCustomerController(reference) {
             const url = `https://viacep.com.br/ws/${postalCodeValue}/json/`;
             Atlas.request.get(url)
                 .then(function(response) {
-                    console.log(response);                    
                     addressInput.value = response.logradouro;
                     addressComplementInput.value = response.bairro;
                     cityInput.value = response.localidade;
                     stateInput.value = response.uf;
                 })
+        });
+    };
+
+    const bindCustomerTypeSelect = function() {
+
+        personTypeSelect.addEventListener("atlas-select-change", function() {
+            const selectedValue = personTypeSelect.value;
+            const naturalPersonTypeFields = reference.querySelectorAll('.js-natural-person-type-fields');
+            const legalPersonTypeFields = reference.querySelectorAll('.js-legal-person-type-fields');
+            const isNaturalPerson = selectedValue == 'NATURAL';
+            const cpfCnpj = reference.querySelector('.js-cpf-cnpj-field');
+
+            naturalPersonTypeFields.forEach((field) => {
+                field.toggleAttribute('required', isNaturalPerson);
+                if (isNaturalPerson) {
+                    field.showElement();
+                } else {
+                    field.hideElement();
+                }
+            });
+
+            legalPersonTypeFields.forEach((field) => {
+                field.toggleAttribute('required', !isNaturalPerson);
+                if (isNaturalPerson) {
+                    field.hideElement();
+                } else {
+                    field.showElement();
+                }
+            });
+
+            cpfCnpj.setAttribute('label', isNaturalPerson ? "CPF" : "CNPJ");
+            cpfCnpj.setAttribute('mask-alias', isNaturalPerson ? "cpf" : "cnpj");
+
         });
     };
 
@@ -31,4 +68,20 @@ document.addEventListener('AtlasContentLoaded', function() {
     const reference = document.querySelector('.js-create-customer-form');
 
     new CreateCustomerController(reference);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const minAge = 16;
+    const today = new Date();
+    const maxDate = today.getFullYear() - minAge;
+
+    today.setFullYear(maxDate);
+
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+    const year = String(today.getFullYear());
+    const maxDateFormatted = `${day}/${month}/${year}`;
+
+    const birthDate = document.getElementById('birthDate');
+    birthDate.setAttribute('max-date', maxDateFormatted);
 });
