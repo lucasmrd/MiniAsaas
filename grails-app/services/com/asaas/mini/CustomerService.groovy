2573b766f2e4624f016b50ce2441801e5d0b88cd
rapha-asaas <raphael.diogenes@asaas.com.br>
2025-06-17 10:19:43 -0400

    corrigindo o customerService

--- grails-app/services/com/asaas/mini/CustomerService.groovy
+++ grails-app/services/com/asaas/mini/CustomerService.groovy
@@ -23,15 +23,22 @@ public class CustomerService {
 
         Customer customer = new Customer()
         customer.personType = parsedParams.personType
-        customer.name = parsedParams.name
-        customer.companyName = parsedParams.companyName
-        customer.email = parsedParams.email
+
+        if(parsedParams.personType.isNatural()) {
+            customer.name = parsedParams.name
+            customer.birthDate = parsedParams.birthDate
+        }
+
+        if(parsedParams.personType.isLegal()) {
+            customer.companyCreationDate =  parsedParams.companyCreationDate
+            customer.companyName = parsedParams.companyName
+            customer.companyType = parsedParams.companyType
+        }
+
         customer.cpfCnpj = parsedParams.cpfCnpj
-        customer.companyType = parsedParams.companyType
+        customer.email = parsedParams.email
         customer.phone = parsedParams.phone
         customer.mobilePhone = parsedParams.mobilePhone
-        customer.birthDate = parsedParams.birthDate
-        customer.companyCreationDate = parsedParams.companyCreationDate
         customer.postalCode = parsedParams.postalCode
         customer.address = parsedParams.address
         customer.addressNumber = parsedParams.addressNumber
@@ -47,39 +54,39 @@ public class CustomerService {
     private Customer validateCustomer(Map parsedParams) {
         Customer customer = new Customer()
 
-        if(parsedParams.personType.isNatural()) {
-            if(!Validator.isValidName(parsedParams.name)) {
-                customer.errors.rejectValue("name", null, "Nome inválido!")        
-            }
-            
-            if(!DateUtil.isValidBirthDate(parsedParams.birthDate)) {
-                customer.errors.rejectValue("birthDate", null, "Data de nascimento inválida!")
-            }
+        if (parsedParams.personType) {
+            if (parsedParams.personType.isNatural()) {
+                if (!Validator.isValidName(parsedParams.name)) {
+                    customer.errors.rejectValue("name", null, "Nome inválido!")        
+                }
+                
+                if (!DateUtil.isValidBirthDate(parsedParams.birthDate)) {
+                    customer.errors.rejectValue("birthDate", null, "Data de nascimento inválida!")
+                }
+            if (parsedParams.personType.isLegal()) {
+                if (!Validator.isValidCompanyName(parsedParams.companyName)) {
+                    customer.errors.rejectValue("companyName", null, "Nome da Empresa inválida!")
+                }
+            }}
         }
 
-        if(parsedParams.personType.isLegal()) {
-            if (!Validator.isValidCompanyName(parsedParams.companyName)) {
-                customer.errors.rejectValue("companyName", null, "Nome da Empresa inválida!")
-            }
-        }
-
-        if(!Validator.isValidEmail(parsedParams.email)) {
+        if (!Validator.isValidEmail(parsedParams.email)) {
             customer.errors.rejectValue("email", null, "Email inválido!")
         }
 
-        if(!Validator.isValidCpfCnpj(parsedParams.cpfCnpj)) {
+        if (!Validator.isValidCpfCnpj(parsedParams.cpfCnpj)) {
             customer.errors.rejectValue("cpfCnpj", null, "CPF/CNPJ inválido!")
         }
 
-        if(!Validator.isValidPhone(parsedParams.phone)) {
+        if (!Validator.isValidPhone(parsedParams.phone)) {
             customer.errors.rejectValue("phone", null, "Telefone inválido!")
         }
 
-        if(!Validator.isValidMobilePhone(parsedParams.mobilePhone)) {
+        if (!Validator.isValidMobilePhone(parsedParams.mobilePhone)) {
             customer.errors.rejectValue("mobilePhone", null, "Celular inválido!")
         }
 
-        if(!Validator.isValidPostalCode(parsedParams.postalCode)) {
+        if (!Validator.isValidPostalCode(parsedParams.postalCode)) {
             customer.errors.rejectValue("postalCode", null, "CEP inválido!")
         }
 
