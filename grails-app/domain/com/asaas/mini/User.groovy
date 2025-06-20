b02b20325587814aa38a8742a701b6b5e2cdb4e3
Lucas Miranda <lucas.mranda1@gmail.com>
2025-06-20 10:44:17 -0300

    Criando relacionamento entre User e Customer

--- grails-app/domain/com/asaas/mini/User.groovy
+++ grails-app/domain/com/asaas/mini/User.groovy
@@ -12,10 +12,17 @@ class User implements Serializable {
     private static final long serialVersionUID = 1
 
     String username
+
     String password
+
+    Customer customer
+
     boolean enabled = true
+
     boolean accountExpired
+
     boolean accountLocked
+
     boolean passwordExpired
 
     Set<Role> getAuthorities() {
@@ -25,9 +32,11 @@ class User implements Serializable {
     static constraints = {
         password nullable: false, blank: false, password: true
         username nullable: false, blank: false, unique: true
+        customer unique: true, nullable: false
     }
 
     static mapping = {
-	    password column: '`password`'
+	    table 'users'
+        password column: '`password`'
     }
 }
