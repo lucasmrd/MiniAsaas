34069925d6844de292c8e997af3a9be9e7cf12f1
rapha-asaas <raphael.diogenes@asaas.com.br>
2025-06-24 12:50:38 -0400

    adicionando relacionamento customer x payer

--- grails-app/domain/com/asaas/mini/Payer.groovy
+++ grails-app/domain/com/asaas/mini/Payer.groovy
@@ -1,5 +1,8 @@
 package com.asaas.mini
 
 import com.asaas.mini.utils.BaseEntityPersonInfo
+import com.asaas.mini.Customer
 
-public class Payer extends BaseEntityPersonInfo { }
+public class Payer extends BaseEntityPersonInfo {
+    Customer customer
+ }
