223d6c3fce48268e19b96edefb68efaa9e512108
rapha-asaas <raphael.diogenes@asaas.com.br>
2025-06-25 09:49:18 -0400

    adicionando os métodos index e save para persistir os dados no banco

--- grails-app/controllers/com/asaas/mini/PayerController.groovy
+++ grails-app/controllers/com/asaas/mini/PayerController.groovy
@@ -1,5 +1,22 @@
 package com.asaas.mini
 
-import com.asaas.mini.PayerService
+public class PayerController {
 
-public class PayerController { }
+    def payerService
+    def springSecurityService
+
+    def index() {
+        println "${springSecurityService.currentUser.customer}"
+    }
+
+    def save() {
+        try {
+            params.customer = springSecurityService.currentUser.customer
+            Payer payer = payerService.save(params)
+            render "Sucesso ao salvar pagador: ${payer.name} com ID: ${payer.id}"
+        } catch (Exception e) {
+            println "Error occurred: ${e.message}"
+            redirect(action: "index", params: [error: "Failed to save payer"])
+        }
+     }
+}
