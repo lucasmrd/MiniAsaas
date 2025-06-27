809bf2d29d50d28c210e0edcf2834c06b7a0a4e9
rapha-asaas <raphael.diogenes@asaas.com.br>
2025-06-27 08:50:04 -0400

    Revert "adicionando método show pós save"
    This reverts commit b7d0539c698c463de70a950ec84344158d66218b.

--- grails-app/controllers/com/asaas/mini/PayerController.groovy
+++ grails-app/controllers/com/asaas/mini/PayerController.groovy
@@ -13,18 +13,10 @@ public class PayerController {
         try {
             params.customer = springSecurityService.currentUser.customer
             Payer payer = payerService.save(params)
-            redirect(action: "show", id: payer.id)
+            render "Sucesso ao salvar pagador: ${payer.name} com ID: ${payer.id}"
         } catch (Exception e) {
             println "Error occurred: ${e.message}"
             redirect(action: "index", params: [error: "Failed to save payer"])
         }
-    }
-
-    def show() {
-        Payer payer = Payer.get(params.id)
-
-        if (payer) return [payer: payer]
-
-        render "Pagador não encontrado"
-    }
+     }
 }
