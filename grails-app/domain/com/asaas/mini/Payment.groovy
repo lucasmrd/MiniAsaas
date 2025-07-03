a304ca95d776c2e2d7e88578c1cd8f4a004bd2fd
Lucas Miranda <lucas.mranda1@gmail.com>
2025-07-02 22:55:32 -0300

    Entidade Payment criada

---
+++ grails-app/domain/com/asaas/mini/Payment.groovy
@@ -0,0 +1,31 @@
+package com.asaas.mini
+
+import com.asaas.mini.enums.BillingType
+import com.asaas.mini.enums.PaymentStatus
+import com.asaas.mini.utils.BaseEntity
+
+class Payment extends BaseEntity {
+
+    public static final BigDecimal MIN_VALUE = 5.00
+
+    Customer customer
+
+    Payer payer
+
+    BillingType billingType
+
+    PaymentStatus status
+
+    BigDecimal value
+
+    String description
+
+    Date dueDate
+
+    Date paymentDate
+
+    static constraints = {
+        description nullable: true, blank: true
+        paymentDate nullable: true
+    }
+}
