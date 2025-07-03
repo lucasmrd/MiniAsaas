5ee1f2edcb6ac295b693647192d89bf05b45c802
Lucas Miranda <lucas.mranda1@gmail.com>
2025-07-02 22:57:45 -0300

    Enum BillingType criado

---
+++ src/main/groovy/com/asaas/mini/enums/BillingType.groovy
@@ -0,0 +1,35 @@
+package com.asaas.mini.enums
+
+import grails.util.Holders
+
+enum BillingType {
+
+    BANK_SLIP,
+    PIX,
+    CREDIT_CARD,
+    UNDEFINED
+
+    public String getLabel() {
+        return Holders.applicationContext
+                .getBean("messageSource")
+                .getMessage(
+                        "billingType.${this.toString()}.label",
+                        null,
+                        "",
+                        new Locale("pt","BR"))
+    }
+
+    public static BillingType convert(billingType) {
+        try {
+            def value = billingType
+
+            if (value instanceof List || value instanceof String[]) value = value[0]
+
+            if (value instanceof String) value = value.toUpperCase()
+
+            return value as BillingType
+        } catch(Exception e) {
+            return null
+        }
+    }
+}
